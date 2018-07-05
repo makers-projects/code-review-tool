4f4db11614d84c3f4124bbc4b25c2c1ce905637d
Lorraine Philllips <ljcphillips@gmail.com>
2018-07-05 17:35:29 +0100

    spiking codes

--- app/controllers/code_controller.rb
+++ app/controllers/code_controller.rb
@@ -1,4 +1,15 @@
 class CodeController < ApplicationController
   def index
+    @codes = Code.all.order('updated_at DESC')
+  end
+
+  def new
+    @code = codes.build
+  end
+
+  private
+
+  def code_params
+    params.require(:code).permit(:content)
   end
 end
